1941d9ec8be9c84d2fedc7f87f3856326ccf438c
Ryan Murry <ryanjmurry@gmail.com>
2018-08-03 14:57:36 -0700

    add test for yearsToSeconds function

--- spec/galactic-calculator-spec.js
+++ spec/galactic-calculator-spec.js
@@ -9,4 +9,10 @@ describe('GalacticCalculator', function() {
         expect(newGalacticCalculator.age).toEqual(5);
         expect(newGalacticCalculator.birthday).toEqual(birthday);
       });
+
+      it('should convert an age, in years, to seconds', function() {
+        let birthday = new Date("August 2, 2018");
+        let newGalacticCalculator = new GalacticCalculator("M", 5, birthday);
+        expect(newGalacticCalculator.yearsToSeconds()).toEqual(157788000);
+      });
 });
