8fb93e44be5663c121d00b2c564e0887f8a56e76
Ryan Murry <ryanjmurry@gmail.com>
2018-08-03 14:55:14 -0700

    test for GalacticCalculator constructor

---
+++ spec/galactic-calculator-spec.js
@@ -0,0 +1,12 @@
+import { GalacticCalculator } from "../src/galactic-calculator.js";
+
+describe('GalacticCalculator', function() {
+
+    it('should test GalacticCalculator Constructor', function() {
+        let birthday = new Date("August 2, 2018");
+        let newGalacticCalculator = new GalacticCalculator("M", 5, birthday);
+        expect(newGalacticCalculator.sex).toEqual("M");
+        expect(newGalacticCalculator.age).toEqual(5);
+        expect(newGalacticCalculator.birthday).toEqual(birthday);
+      });
+});
