30e185022537f1a94e8322c3d6027858863927a7
Ryan Murry <ryanjmurry@gmail.com>
2018-08-03 15:19:58 -0700

    add ageSecondCalculator function to determine how old a user is in seconds from a given birthday

--- src/galactic-calculator.js
+++ src/galactic-calculator.js
@@ -1,21 +1,24 @@
 export class GalacticCalculator {
-    constructor(sex, age, birthday) {
-        this.sex = sex;
-        this.age = age;
-        this.birthday = birthday;
-        this.planets = {
-            "Earth": 1,
-            "Mercury": 0.24,
-            "Venus": 0.62,
-            "Mars": 1.88,
-            "Jupiter": 11.86
-        }
+  constructor(sex, age, birthday) {
+    this.sex = sex;
+    this.age = age;
+    this.birthday = birthday;
+    this.planets = {
+      "Earth": 1,
+      "Mercury": 0.24,
+      "Venus": 0.62,
+      "Mars": 1.88,
+      "Jupiter": 11.86
     }
+  }
 
-    yearsToSeconds() {
-        let secondsInAYear = 365.25 * 24 * 60 * 60;
-        return this.age * secondsInAYear;
-    }
+  yearsToSeconds() {
+    let secondsInAYear = 365.25 * 24 * 60 * 60;
+    return this.age * secondsInAYear;
+  }
 
-    
+  ageSecondCalculator() {
+    let today = new Date();
+    return (today.getTime() - this.birthday.getTime()) / 1000;
+  }
 }
